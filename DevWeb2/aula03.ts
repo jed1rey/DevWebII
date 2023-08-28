9428151c3044487a95e09cc7c695e31032a10534
Paula Abib <104170184+jed1rey@users.noreply.github.com>
2023-08-28 02:27:54 -0300

    Create aula03.ts

---
+++ DevWeb2/aula03.ts
@@ -0,0 +1,26 @@
+namespace exemplo01{
+    //do while - irá executar ao menos uma vez.
+    let num: number = 18
+    let i: number = 0;
+    do {
+        //console.log("entrou no DO WHILE");
+        console.log(i)
+        i++;  //i = i + 1
+        num = i == 10? 19 : 18;
+    } while (num <= 18) 
+    
+    
+    //while
+    while (i<=20) {
+        console.log (i);
+        i++;
+    }
+      
+
+    //for 
+
+    for (i = 21; i <= 30; i++){
+        console.log(i);
+        
+    }
+    }
